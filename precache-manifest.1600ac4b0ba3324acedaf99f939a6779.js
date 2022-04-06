6ccbf1016e7d9dc1a3f0bb2a73011110bb5de1f8
bruno <bruno.cami@hotmail.com>
2022-04-06 18:17:26 -0300

    Updates

---
+++ precache-manifest.1600ac4b0ba3324acedaf99f939a6779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7998b39c4be55f6ff7ef07a45cb707a1",
+    "url": "/tours_app/index.html"
+  },
+  {
+    "revision": "c9023140d3e1ae733ff1",
+    "url": "/tours_app/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/tours_app/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours_app/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9023140d3e1ae733ff1",
+    "url": "/tours_app/static/js/main.1014c85f.chunk.js"
+  },
+  {
+    "revision": "cf70ede6085373832626",
+    "url": "/tours_app/static/js/runtime-main.004e52e0.js"
+  }
+]);
